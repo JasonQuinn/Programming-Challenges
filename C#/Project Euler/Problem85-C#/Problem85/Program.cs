68dfd3864668a7abe18ced705cdc979eebcde196
Jason Quinn <jasonquinn351@gmail.com>
2013-05-10 12:03:15 +0100

    Adding question to problem 85

--- C#/Project Euler/Problem85-C#/Problem85/Program.cs
+++ C#/Project Euler/Problem85-C#/Problem85/Program.cs
@@ -3,6 +3,8 @@ using System.Diagnostics;
 
 namespace Problem85
 {
+    //By counting carefully it can be seen that a rectangular grid measuring 3 by 2 contains eighteen rectangles:
+    //Although there exists no rectangular grid that contains exactly two million rectangles, find the area of the grid with the nearest solution.
     class Program
     {
         static void Main(string[] args)
