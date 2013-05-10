3697ecb8497f8d4806be02c09d500a7450e112c2
Jason Quinn <jasonquinn351@gmail.com>
2013-05-10 11:57:49 +0100

    Changing problem 85

--- C#/Project Euler/Problem85-C#/Problem85/Program.cs
+++ C#/Project Euler/Problem85-C#/Problem85/Program.cs
@@ -10,39 +10,17 @@ namespace Problem85
             var timer = new Stopwatch();
             timer.Start();
 
-            //var closestNumber = int.MaxValue;
-            //var closestX = -1;
-            //var closestY = -1;
-
-            //for (var x = 1; x <= 2000; x++)
-            //{
-            //    for (var y = 1; y <= 2000; y++)
-            //    {
-            //        var currentNumber = GetNumberOfRectanges(x, y);
-            //        if (currentNumber > 2000000)
-            //        {
-            //            if (currentNumber < closestNumber)
-            //            {
-            //                closestNumber = currentNumber;
-            //                closestX = x;
-            //                closestY = y;
-            //            }
-            //            break;
-            //        }
-            //    }
-            //}
-
-
             var smallestDifference = int.MaxValue;
             var closestX = -1;
             var closestY = -1;
 
             for (var x = 1; x <= 2000; x++)
             {
+                var contunue = true;
                 for (var y = 1; y <= 2000; y++)
                 {
                     var currentNumber = GetNumberOfRectanges(x, y);
-                    var currentDifference = Math.Abs(2000000 - currentNumber);
+                    var currentDifference = FindDifference(2000000, currentNumber);
                     if (currentDifference < smallestDifference)
                     {
                         smallestDifference = currentDifference;
@@ -51,12 +29,15 @@ namespace Problem85
                     }
                     if (currentNumber > 2000000)
                     {
+                        contunue = false;
                         break;
                     }
                 }
+                if (!contunue)
+                {
+                    break;
+                }
             }
-
-
 
             timer.Stop();
 
@@ -65,6 +46,23 @@ namespace Problem85
             Console.ReadLine();
         }
 
+        /// <summary>
+        /// Find the difference between two numbers
+        /// </summary>
+        /// <param name="n1"></param>
+        /// <param name="n2"></param>
+        /// <returns></returns>
+        static int FindDifference(int n1, int n2)
+        {
+            return Math.Abs(n1 - n2);
+        }
+
+        /// <summary>
+        /// Get the number of rectanges in a grid
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
         static int GetNumberOfRectanges(int x, int y)
         {
             return (x*(x + 1)*y*(y + 1))/4;
